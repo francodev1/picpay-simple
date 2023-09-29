33b5442566ad2621f917078f0ab9abf4da086e37
Lucas Franco Baptista <lucas.baptista@true.com.br>
2023-09-29 09:57:51 -0300

    feature

---
+++ src/main/java/com/franco/picpaytransaction/PicpayTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.franco.picpaytransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franco/picpaytransaction/PicpayTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franco.picpaytransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
